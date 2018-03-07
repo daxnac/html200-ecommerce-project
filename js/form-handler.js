b366b15bb599628431f4a8f746acec186d470f4d
Andro D Nacu <andro.d.nacu@boeing.com>
2018-03-06 19:59:15 -0800

    Updated JS for Cart handling

--- js/form-handler.js
+++ js/form-handler.js
@@ -7,3 +7,25 @@ function getEmail() {
   feedText.innerHTML = "Thank you " + emailForm + "." + " You will now receive email updates from Winter Wear Warehouse."
 }
 
+var cart = []
+// var price = []
+function addCart(item,itemPrice) {
+    var i = cart.indexOf(item)
+//  var p = price.indexOf(itemPrice)
+    if (i == -1) {
+        cart.push(item)
+//      price.push(itemPrice)
+        console.log(item + " for $" + itemPrice + " has been added to cart.")
+    } else {
+      cart.splice(item)
+//    price.splice(itemPrice)
+      console.log(item + " removed from cart.")
+    }
+}
+
+function viewCart() {
+  for (i = 0; i < cart.length; i++) {
+    console.log(cart[i])
+  }
+  console.log("You have " + cart.length + " total items in your cart.")  
+}
