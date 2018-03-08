b4bf91e8ef8a25372a99b5accd6702a9d1f3cb22
Andro D Nacu <andro.d.nacu@boeing.com>
2018-03-07 21:01:19 -0800

    JS update to addcart

--- js/form-handler.js
+++ js/form-handler.js
@@ -8,19 +8,22 @@ function getEmail() {
 }
 
 var cart = []
+
 // Add items to cart
 function addCart(item,itemPrice) {
-    var i = cart.indexOf(item)
-    if (i == -1) {
-        cart.push(item)
-        console.log(item + " for $" + itemPrice + " has been added to cart.")
-    } else {
-      cart.splice(item)
-      console.log(item + " removed from cart.")
-    }
+  var i = cart.indexOf(item)
+  if (i == -1) {
+      cart.push(item)
+      console.log(item + " for $" + itemPrice + " has been added to cart.")
+  } else {
+    cart.splice(item, 1)
+    console.log(item + " removed from cart.")
+  }
 }
+
 // Display Items in Cart
 function viewCart() {
+  console.log("====Items in Cart====")
   for (i = 0; i < cart.length; i++) {
     console.log(cart[i])
   }
